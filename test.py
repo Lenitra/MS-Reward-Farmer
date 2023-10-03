104f3b9a47b33e05228518f029d3d401f13a33e1
TauMah <lenitramc@gmail.com>
2023-10-04 01:37:00 +0200

    Index meta ?

--- test.py
+++ test.py
@@ -2,6 +2,16 @@
 from selenium import webdriver
 import time
 
-driver = webdriver.Chrome()
+print("Ouverture d'un navigateur en mode mobile")
+userAgent = "Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Mobile Safari/537.36"
+profile = webdriver.ChromeOptions()
+profile.add_argument(f'user-agent={userAgent}')
+# mode mobile emulator devtools
+profile.add_experimental_option("mobileEmulation", {"deviceName": "Samsung Galaxy S8+"})
+
+driver = webdriver.Chrome(options=profile)
+driver.set_window_size(414, 896)
+
 driver.get("https://lenitra.github.io/MS-Reward-Farmer/")
-time.sleep(120)
+while True:
+    print()
